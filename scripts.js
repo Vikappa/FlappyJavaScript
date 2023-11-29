48069afa5f8d56ed5ee8a0e2c5ec9b30106d534e
Vikappa <vincenzo.e.costantini@gmail.com>
2023-11-29 01:23:08 +0100

    Sfondo e bird funzionanti. Inizio scrittura CSS

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// var bird = document.getElementById('bird');
+// var birdPosition = { top: 50, left: 50 }; // Posizione iniziale
+
+// document.addEventListener('keydown', function (event) {
+//     // Puoi aggiungere condizioni per gestire diverse tasti
+//     if (event.key === 'ArrowUp') {
+//         birdPosition.top -= 10; // Muovi verso l'alto
+//     } else if (event.key === 'ArrowDown') {
+//         birdPosition.top += 10; // Muovi verso il basso
+//     } else if (event.key === 'ArrowLeft') {
+//         birdPosition.left -= 10; // Muovi a sinistra
+//     } else if (event.key === 'ArrowRight') {
+//         birdPosition.left += 10; // Muovi a destra
+//     }
+
+//     // Aggiorna la posizione dell'uccellino
+//     bird.style.top = birdPosition.top + '%';
+//     bird.style.left = birdPosition.left + '%';
+// });
